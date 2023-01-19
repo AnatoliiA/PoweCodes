b96e282ffbd346ed6f84c65f280abe8902a5d240
kamarali <kamaralia@gmail.com>
2023-01-19 10:38:16 +0100

    Blog changes capital Blog changes IT Contacts Created Site Change site 3 tests 6

--- netlify/edge-functions/get_movies/get_movies.js
+++ netlify/edge-functions/get_movies/get_movies.js
@@ -1,5 +1,17 @@
-export default async (request) => {
-  return new Response('Hello, World!', {
-    headers: { 'content-type': 'text/html' },
-  })
+const { MongoClient } = require("mongodb");
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
+
+const clientPromise = mongoClient.connect();
+
+const handler = async (event) => {
+  try {
+    const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+    const collection = database.collection(process.env.MONGODB_COLLECTION);
+    // Function logic here ...
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
 }
+
+module.exports = { handler }
